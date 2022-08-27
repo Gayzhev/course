eb683fae705e318695ffd3e4e1feb221a74878fa
Gayzhev <gayzhev@gmail.com>
2022-08-27 10:03:02 +0300

    Home work

--- src/Course.java
+++ src/Course.java
@@ -1,5 +1,116 @@
+
+import java.util.Scanner;
+
 public class Course {
     public static void main(String[] args) {
+//      int value = 5;
+//      int value2 = 3;
+//      int sum = value % value2;
+//      System.out.print(sum);
+//      int value=5;
+//      value = value + 5;
+//      System.out.println(value);
+//      value --;
+//              System.out.println(value);
+
+//      int value = 10;
+//      boolean test = value > 0;
+//      System.out.println(test);
+
+
+        //      Написати програму, яка буде отримувати від користувача два цілих числа та виводити на екран їхню суму.
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("введите число 1:");
+      int value = scan.nextInt();
+      System.out.println("введите число 2:");
+      int value2 = scan.nextInt();
+//
+//      System.out.println("сумма введеного  " + (value + value2));
+
+
+//      2. Написати програму, яка буде отримувати від користувача два числа a та b типу double та виводити на екран результат наступних операцій:
+//      1. a + b
+//      2. a - b
+//      3. a * b
+//      4. a / b
+
+//        Scanner scan = new Scanner(System.in);
+//        System.out.println("введите число 1:");
+//        double value = scan.nextDouble();
+//        System.out.println("введите число 2:");
+//        double value2 = scan.nextDouble();
+//        System.out.println("сумма введеного  " + (value + value2));
+//        System.out.println("разница введеного  " + (value - value2));
+//        System.out.println("произведение введеного  " + (value * value2));
+//        System.out.println("деление введеного  " + (value / value2));
+
+//        Написати програму, яка буде запитувати в користувача радіус кола та виводити на екран його площу та довжину
+
+//            Scanner scan = new Scanner(System.in);
+//            System.out.println("Введите радиус круга:");
+//        double radius = scan.nextDouble();
+//        double area = 3.14 * (radius * radius);
+//            System.out.println("Площадь круга: " + area);
+//        double perimeter= 3.14 * 2 * radius;
+//            System.out.println("Длина окружности: " + perimeter) ;
+
+//        Написати програму, яка буде запитувати в користувача одне ціле число та потім виводити таблицю множення
+//        для цього числа від 1 до 10. Наприклад, якщо користувач ввів 8:
+//
+//        Scanner scan = new Scanner(System.in);
+//      System.out.println("введите число:");
+//            int a = scan.nextInt();
+//        for (int i=0; i< 10; i++){
+//            System.out.println(a + " x " + (i+1) + "=" +
+//                    (a * (i+1)));
+
+
+//        Написати програму, яка буде запитувати в користувача якесь ціле число і виводити на екран
+//        кількість цифр в цьому числі (підказка: використати ділення на 10)
+//        Scanner scan = new Scanner(System.in);
+//        System.out.println("введите число:");
+//        int a = scan.nextInt();{
+//        if ((a % 10) == a) {System.out.println("1 цифра")}
+
+        // второй вариант
+
+//        Scanner scan = new Scanner(System.in);
+//       System.out.println("введите число:");
+//        int a = scan.nextInt();
+//        while (a > 0) {
+//            int b = a % 10;
+//            System.out.print(b +",");
+//            a= a/10;
+
+//        Написати програму, яка буде запитувати в користувача три цілих числа: a, b і c.
+//        Якщо a + b = c, то вивести на екран true, інакше - false.
+
+//        Scanner scan = new Scanner(System.in);
+//        System.out.println("введите число1:");
+//        int a = scan.nextInt();
+//        System.out.println("введите число2:");
+//        int b = scan.nextInt();
+//        System.out.println("введите число3:");
+//        int c = scan.nextInt();
+//       if (a+b==c) {
+//           System.out.println(true);
+//        else {
+//               System.out.println(false);
+
+
+//        Написати програму, яка запитує в користувача ввести одне слово і виводить на екран
+//        це слово в зворотному порядку (підказка: використати метод toCharArray із класу String)
+
+//        System.out.println("enter the word :");
+//        Scanner sc = new Scanner(System.in);
+//        char c = sc.next().charAt(0);
+//        System.out.println("c = "+c);
+
+//        char word = scan.next().charAt;
+//       char[] word = s.toCharArray();
+//        for (int i = 0; i < word.length; i++) {
+//            System.out.println(word[i]);
+        }
     }
-    System.out.println("Hello, World!");
-}
+
