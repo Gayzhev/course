aeebd89fccd53e35ebb27d915b128123b02aadc3
Gayzhev <gayzhev@gmail.com>
2022-09-04 15:31:25 +0300

    Home work

---
+++ src/ArrayHomework/Main.java
@@ -0,0 +1,56 @@
+package ArrayHomework;
+
+
+public class Main {
+
+        public static void main(String[] args) {
+
+//            int[] newArray = {100, 194, 10, 521, 1, 18, 12, 10, 20};
+//            Arrays.sort(newArray);
+//            System.out.println("sorted array is: " + Arrays.toString(newArray));
+//            int maxvalue=newArray[newArray.length-1];
+//            int minvalue=newArray[0];
+//            System.out.println("Maximum is  " + maxvalue);
+//            System.out.println("Maximum is  " + minvalue);
+
+//            int[] newArray = {85,15,8,48,99};
+//            int a = newArray[0];
+//            int b = newArray[newArray.length-1];
+//            if  a=b ;{
+//                System.out.println("Maximum is  " + maxvalue);
+//            } else {
+//                    System.out.println("Maximum is  " + maxvalue);
+//                }
+
+
+//5. Написати програму, яка буде перевіряти, чи перший і останній елемент масиву цілих чисел рівні між собою.
+//        Програма має додати перевірку, що масив чисел має розмір від 2 і більше елементів.
+
+            int[] newArray = {100, 194, 10, 521, 1, 18, 12, 10, 90};
+            int a = newArray[0];
+            int b = newArray[newArray.length - 1];
+
+            if (a == b) {
+                System.out.println("равны");}
+            else{
+                    System.out.println("первый и последний элементы - не равны");
+
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/project_loyalty/Visit.java
@@ -0,0 +1,29 @@
+
+package project_loyalty;
+
+
+public class Visit {
+
+    private String Customer;
+    private double ServiceExpense;
+    private double ProductExpense;
+
+    private String Calendar;
+
+    public Visit(String customer, double serviceExpense, double productExpense, String calendar) {
+        Customer = customer;
+        ServiceExpense = serviceExpense;
+        ProductExpense = productExpense;
+        Calendar = calendar;
+    }
+
+
+
+//    public Calendar getCalendar() {
+//        return Calendar;
+    }
+
+//    {
+//                System.out.print(Calendar1.getCalendar);
+//    }
+//}
