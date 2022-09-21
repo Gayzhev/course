3c24204303663bdcd5763368b34a46b309074737
Gayzhev <gayzhev@gmail.com>
2022-09-21 23:58:50 +0300

    update 21-09 new

--- src/Homework3.java
+++ src/Homework3.java
@@ -1,11 +1,31 @@
 import java.util.Scanner;
 
 public class Homework3 {
-        public static void main(String [] args) {
-            Scanner scan = new Scanner(System.in);
-            System.out.println("Введите длину в дюймах: ");
-            double inch = scan.nextInt();
-            double lenght = inch * 2.54;
-            System.out.println("Ваша длина в сантиметрах: " + lenght + "cm");
+    public static void main(String[] args) {
+//            Scanner scan = new Scanner(System.in);
+//            System.out.println("Введите длину в дюймах: ");
+//            double inch = scan.nextInt();
+//            double lenght = inch * 2.54;
+//            System.out.println("Ваша длина в сантиметрах: " + lenght + "cm");
+//        }
+//    }
+//
+        double max;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Input first double: ");
+        double dou1 = scan.nextInt();
+        System.out.println("Input second double: ");
+        double dou2 = scan.nextInt();
+        System.out.println("Input third double: ");
+        double dou3 = scan.nextInt();
+            if(dou1>dou2)
+        max=dou1;
+            else max=dou2;
+            if(max>dou3)
+        System.out.println("Максимальное число: " + max);
+            else
+        System.out.println("Максимальное число: " + dou3);
         }
-    }
+}
+
+
